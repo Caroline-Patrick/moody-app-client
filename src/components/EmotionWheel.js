cd91c4e8687a1fa1d137050af52e75e6aa9ee7d4
Caroline-Patrick <carolinespatrick@gmail.com>
2023-04-15 22:52:59 -0500

    emotionwheel sending data to logform, which is connecting to backend

--- src/components/EmotionWheel.js
+++ src/components/EmotionWheel.js
@@ -1,57 +1,60 @@
-import React, { useRef, useEffect, useState } from 'react';
-import * as am4core from '@amcharts/amcharts4/core';
-import * as am4charts from '@amcharts/amcharts4/charts';
-import * as am4plugins_sunburst from '@amcharts/amcharts4/plugins/sunburst';
-import am4themes_animated from '@amcharts/amcharts4/themes/animated';
+import React, { useRef, useEffect, useState } from "react";
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import * as am4plugins_sunburst from "@amcharts/amcharts4/plugins/sunburst";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import emotionList from "../emotionList";
 
-import { LogFormComponent } from './LogFormComponent';
+import { LogFormComponent } from "./LogFormComponent";
 
 am4core.useTheme(am4themes_animated);
 
 export const EmotionWheel = () => {
   const chartRef = useRef(null);
 
-const [formVisible, setFormVisible] = useState(false);
-const [selectedChartData, setSelectedChartData] = useState(null);
+  const [formVisible, setFormVisible] = useState(false);
+  const [selectedChartData, setSelectedChartData] = useState(null);
 
-const handleSliceClick = (event) => {
+  const handleSliceClick = (event) => {
     const data = event.target.dataItem.dataContext;
     setSelectedChartData(data);
     setFormVisible(true);
+
   };
+
+
 
   useEffect(() => {
     if (emotionList) {
-      const chart = am4core.create('chartdiv', am4plugins_sunburst.Sunburst);
+      const chart = am4core.create("chartdiv", am4plugins_sunburst.Sunburst);
       chart.data = emotionList;
       chart.padding(0, 0, 0, 0);
       chart.radius = am4core.percent(100);
 
       chart.colors.step = 2;
       chart.fontSize = 14;
-      
-    //   chart.innerRadius = am4core.percent(20);
+
+      //   chart.innerRadius = am4core.percent(20);
 
       // Define data fields
-      chart.dataFields.value = 'value';
-      chart.dataFields.name = 'name';
-      chart.dataFields.children = 'children';
-      chart.dataFields.color = 'color';
-      chart.dataFields.desc = 'desc';
+      chart.dataFields.value = "value";
+      chart.dataFields.name = "name";
+      chart.dataFields.children = "children";
+      chart.dataFields.color = "color";
+      chart.dataFields.desc = "desc";
 
       let level0SeriesTemplate = new am4plugins_sunburst.SunburstSeries();
       level0SeriesTemplate.hiddenInLegend = false;
-    //   chart.seriesTemplates.setKey('0', level0SeriesTemplate);
+      //   chart.seriesTemplates.setKey('0', level0SeriesTemplate);
 
-    var level0 = chart.seriesTemplates.create("0");
-    var level1 = chart.seriesTemplates.create("1");
-    var level2 = chart.seriesTemplates.create("2");
+      var level0 = chart.seriesTemplates.create("0");
+      var level1 = chart.seriesTemplates.create("1");
+      var level2 = chart.seriesTemplates.create("2");
 
       // Set default behavior for all series
-      level0.labels.template.text = '{name}';
-      level1.labels.template.text = '{name}';
-      level2.labels.template.text= '{name}';
+      level0.labels.template.text = "{name}";
+      level1.labels.template.text = "{name}";
+      level2.labels.template.text = "{name}";
       level0SeriesTemplate.tooltip.defaultState.properties.opacity = 0;
       level0SeriesTemplate.tooltip.animationDuration = 0;
       level0SeriesTemplate.strokeOpacity = 1;
@@ -60,28 +63,27 @@ const handleSliceClick = (event) => {
       level2.slices.template.tooltipText = "";
       level2.slices.template.events.on("hit", handleSliceClick);
 
-
-    //   chart.legend = new am4charts.Legend();
-    //   chart.legend.labels.template.text = '{name}';
-    //   chart.legend.valueLabels.template.text = '{value}';
+      //   chart.legend = new am4charts.Legend();
+      //   chart.legend.labels.template.text = '{name}';
+      //   chart.legend.valueLabels.template.text = '{value}';
 
       chartRef.current = chart;
       return () => {
         chart.dispose();
-    
       };
     }
   }, [emotionList]);
 
   return (
-  <div id="chartdiv" style={{ width: '100%', height: '1000px' }}>
-    <LogFormComponent 
+    <div
+      id="chartdiv"
+      style={{ width: "100%", height: "1000px" }}
+    >
+      <LogFormComponent
         visible={formVisible}
         data={selectedChartData}
-        onHide={()=>setFormVisible(false)}
-        />
-  </div>
-  )
-}
-
-
+        onHide={() => setFormVisible(false)}
+      />
+    </div>
+  );
+};
--- src/components/LogFormComponent.js
+++ src/components/LogFormComponent.js
@@ -1,34 +1,51 @@
-import React from 'react'
-
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { Container, Button, TextField } from '@mui/material';
+import axios from 'axios';
 
 export const LogFormComponent = ({ visible, data, onHide }) => {
-  
-    if (!visible) {
-        return null;
-      }
-    
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        // Handle form submission
-        onHide();
-      };
+  const [description, setDescription] = useState('');
+  const moodName = data ? data.name : '';
 
-    return (
-        <div>
-      <form onSubmit={handleSubmit}>
-      <input
-                className="input"
-                type="text"
-                value={data.name}
-                onChange={((e)=>{
-                   console.log(e.target.value)
-                  
-                })}
-                 />
-       
-        <button type="submit">Submit</button>
-      </form>
-      <button onClick={onHide}>Close</button>
+  useEffect(() => {
+    if (visible && moodName) {
+      axios
+        .get(`http://localhost:5000/log/moods/tier3/${moodName}`)
+        .then((response) => {
+          setDescription(response.data[0].subSubMoodDesc)
+        });
+    }
+  }, [visible, moodName]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission
+    onHide();
+  };
+
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <div>
+      <Container>
+        {moodName}: {description}
+        <form onSubmit={handleSubmit}>
+          <TextField
+            id="filled-basic"
+            label="What are you feeling?"
+            variant="filled"
+          />
+
+          <Button variant="contained" type="submit">
+            Submit
+          </Button>
+        </form>
+        <Button variant="contained" onClick={onHide}>
+          Close
+        </Button>
+      </Container>
     </div>
-  )
+  );
 };
